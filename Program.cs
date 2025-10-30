7a18f93d5995c7db5bceb4ca3d0c2d198049cf8e
ne-utka <metterberetta@gmail.com>
2025-10-30 18:40:45 +0300

    Task

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+// Метод для поиска индекса наименьшего элемента в массиве
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+        int[] arr = numbers.Select(int.Parse).ToArray();
+        int smallestIndex = FindSmallest(arr);
+        Console.WriteLine("Наименьший элемент:  {arr[smallestIndex]}");
+        Console.WriteLine("Индекс наименьшего элемента: {arr[smallestIndex]}");
+        Console.WriteLine("Весь массив: " + string.Join(" ", arr));
+    }
+    
+    static int FindSmallest(int[] arr)
+    {
+        int smallest = arr[0];
+        int smallest_index = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < smallest)
+            {
+                smallest = arr[i];
+                smallest_index = i;
+            }
+        }
+        return smallest_index;
+    }
+}
